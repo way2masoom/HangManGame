6a6fe9c27e039be543d31f5a88b88ebd662a0f63
Way2masoom <way2masoom@gmail.com>
2025-01-06 23:53:49 +0545

    Basic Components Learning

--- 02_FirstViteProject/src/App.jsx
+++ 02_FirstViteProject/src/App.jsx
@@ -1,21 +1,25 @@
 import './App.css'
+// Default Import
+import MyButton from './MyButton'
+
+// Naming import from same component
+import { SecondaryButton } from './MyButton'
 
 function App() {
   return (<div>Hello World <br />
+
     {/* /* Calling button component  */}
     <div id="buttons">
       <MyButton />
       <MyButton />
       <MyButton />
+      <MyButton />
+
+      <SecondaryButton />
     </div>
+
   </div>
   )
-}
-
-
-// Crating an button component
-function MyButton() {
-  return <button>Click Me</button>
 }
 
 export default App
---
+++ 02_FirstViteProject/src/MyButton.jsx
@@ -0,0 +1,12 @@
+
+// Crating an button component
+function MyButton() {
+    return <button>Click Me</button>
+} 
+// Naming Export
+export function SecondaryButton() {
+    return <button>Test Btn</button>
+}
+
+// Default Export
+export default MyButton
