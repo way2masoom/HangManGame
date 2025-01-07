cc1e553fb2641f4d521e7d2baf882805c5131018
Way2masoom <way2masoom@gmail.com>
2025-01-07 11:03:23 +0545

    Added Button type

--- HangmanGame/src/components/Button/Button.jsx
+++ HangmanGame/src/components/Button/Button.jsx
@@ -1,11 +1,11 @@
 // Importing the stylingButton file
 import getButtonStyling from "./getButtonStyling";
 
-function Button({ text, onClickHandler, styleType = "primary" }) {
+function Button({ text, onClickHandler, styleType = "primary", type = "button" }) {
     return (
         <button onClick={onClickHandler}
             // Calling the getButtonStyling Function
-            className={`px-4 py-3 ${getButtonStyling(styleType)} text-white`}
+            className={`px-4 py-3 ${getButtonStyling(styleType)} text-white`} type={type}
         >
 
             {text}
