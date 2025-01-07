8f2bb5aa520c260256b7e6009f15c38884c9bd5b
Way2masoom <way2masoom@gmail.com>
2025-01-07 11:05:16 +0545

    Created Page Folder for Each Section

---
+++ HangmanGame/src/pages/StartGame.jsx
@@ -0,0 +1,11 @@
+// Function to Start the game
+function StartGame() {
+    
+    // React Fragment
+    <> 
+        <h1>Start Game</h1>
+        <h3>End Game</h3>
+    </>
+}
+
+export default StartGame;
