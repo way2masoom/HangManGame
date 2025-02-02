c6e14d932a5ea775cb9915dc00c4736c4e0d38aa
Way2masoom <way2masoom@gmail.com>
2025-02-02 13:48:23 +0530

    setup done of context API[wordList in APP]

--- HangmanGame/src/App.jsx
+++ HangmanGame/src/App.jsx
@@ -3,20 +3,25 @@ import './App.css'
 import StartGame from './pages/StartGame'
 import PlayGame from './pages/PlayGame';
 import Home from './pages/Home';
+import { WordContext } from './Context/WordContext.js'
+import { useState } from 'react';
 
 // import TextInputFormContainer from './components/TextInputForm/TextInputFormContainer';
 
 function App() {
+
+  const [wordList, setWordList] = useState([]);
+
   return (
-    <Routes>
+    <WordContext.Provider value={{ wordList, setWordList }}>
+      <Routes>
 
-      <Route path='/start' element={<StartGame />} />
-      <Route path='/play' element={<PlayGame />} />
-      <Route path='/' element={<Home text="Single Player" />}
-      />
-
-    </Routes>
-
+        <Route path='/start' element={<StartGame />} />
+        <Route path='/play' element={<PlayGame />} />
+        <Route path='/' element={<Home text="Single Player" />}
+        />
+      </Routes>
+    </WordContext.Provider>
 
 
 
--- HangmanGame/src/main.jsx
+++ HangmanGame/src/main.jsx
@@ -2,13 +2,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import { WordContext } from './Context/WordContext.js'
+
 
 
 createRoot(document.getElementById('root')).render(
-  <WordContext.Provider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </WordContext.Provider>
 )
